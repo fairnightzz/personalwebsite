a80bff875dbfbdd998207a72eaf2a725c1a4aa41
Zhehai Zhang <zhehaizhang1@gmail.com>
2021-02-15 12:28:22 -0500

    projects, mobile breakpoints

--- pages/index.vue
+++ pages/index.vue
@@ -7,68 +7,77 @@
     >
     <v-overlay absolute color="black" opacity="0.5">
       <v-row>
-        <v-col cols = "12" >
+        <v-col cols = "8" class="ma-2" >
+          <v-spacer></v-spacer>
           <h1 data-aos="fade-up" class="display-3 font-weight-medium">
           Hi, I'm Zhehai (Mark) Zhang. 
           </h1>   
         </v-col>
-        <v-col cols="12">
+     </v-row>
+      <v-row>
+        <v-col cols="8" class="ma-2">
           <h2 data-aos="flip-up" class ="display-1 font-weight-regular">
             Welcome to my website. 
           </h2>
         </v-col>
+ 
       </v-row>
 
     </v-overlay>
    </v-parallax> 
 
     <v-row justify="center" align="center" >
-      <v-col cols="4" class="ma-2" >
+      <v-col sm="8" xs="8" md="4" lg="4" class="ma-2" >
         <v-card data-aos="fade-up">
           <v-card-text>
-          <h3 class="display-1">I'm a first year student studying Computer Science at the University of Waterloo.</h3>
+          <h3 :class="$vuetify.breakpoint.smAndDown ? 'title' : 'display-1'">I'm a first year student studying Computer Science at the University of Waterloo.</h3>
           </v-card-text>
         </v-card>
       </v-col>
-      <v-col cols="4" class="ma-2">
+      <v-col sm="8" xs="8" md="4" lg="4" class="ma-2">
         <v-img eager src="/explore.jpg">
         </v-img>
       </v-col>
     </v-row>
      <v-row justify="center" align="center" >
-      <v-col cols="4" class="ma-2">
-        <v-carousel cycle hide-delimiters :show-arrows="false">
+      <v-col sm="8" xs="8" md="4" lg="4" class="ma-2">
+        <v-carousel cycle hide-delimiters hide-delimiter-background >
           <v-carousel-item eager  contain src="canoe.jpg"></v-carousel-item>
           <v-carousel-item eager contain src="chess.JPG"></v-carousel-item>
           <v-carousel-item eager contain src="lake.jpg"></v-carousel-item>
         </v-carousel>
       </v-col>
  
-      <v-col cols="4" class="ma-2" >
+      <v-col sm="8" xs="8" md="4" lg="4" class="ma-2" >
         <v-card data-aos="fade-up">
           <v-card-text>
-          <h3 class="display-1">I like trying new things!</h3>
+          <h3 :class="$vuetify.breakpoint.smAndDown ? 'title' : 'display-1'">I like trying new things!</h3>
           </v-card-text>
         </v-card>
       </v-col>
    </v-row>
 
+   <v-row justfiy="center" align="center">
+     <v-col cols="12">
+     <h1 class= "text-center" :class="$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1'">Projects</h1>
+     </v-col>
+   </v-row>
+
    <v-row justify="center" align="center" >
-     <v-container class = "d-flex flex-wrap align-content-space-around">
+     <v-container class = "d-flex flex-wrap align-content-space-around" align="center" justify="center">
         <v-hover v-slot="{hover}" v-for="(project,index) in projects" :key="index">
-          <v-col cols="4">
+          <v-col  xs="8" sm="5" md = "4" lg="4">
             <v-card flat data-aos="flip-up">
               <v-img :src="project.imgsrc" height="300">
                 <v-fade-transition>
-                  <v-overlay v-if="hover" absolute color="pink" opacity="0.5">
-                    <v-card-text  align="center" class="white--text display-1">
+                  <v-overlay v-if="hover" absolute color="purple" opacity="0.8">
+                    <v-card-text  align="center" class="white--text body-1">
                       {{project.description}}
                     </v-card-text>
                   </v-overlay >
                </v-fade-transition>
              </v-img>
               <v-card-title>{{project.name}}</v-card-title>
-              
             </v-card>
           </v-col>
           </v-hover> 
@@ -91,11 +100,12 @@ export default {
   data: () => {
     return {
       projects: [
-        {name: "Tinderwal", description:"Vote based on your faour", imgsrc: "/deca.JPG"},
-        {name: "9it", description: "CLI system", imgsrc: "/hackthenorth.jpg"},
-        {name: "Tinderwal", description:"Vote based on your faour", imgsrc: "/deca.JPG"},
-        {name: "Tinderwal", description:"Vote based on your faour", imgsrc: "/deca.JPG"}
-
+        {name: "Subspace", description:"Won HackTheNorth 2021. Work side-by-side again during COVID? Subspace gives you cloud-based desktops for remote collaboration, allowing you to instantaneously share your computing environment with others.", imgsrc: "/projects/subspace.jpg"},
+        {name: "9it - Version Control System", description: "totally not git! 9it-cli is a lightweight version control system that is intended for individual use.", imgsrc: "/projects/9it.png"},
+        {name: "Tinderwal", description:"Vote for the favourite pictures of your friends, Tinder style!", imgsrc: "/projects/tinderwal.png"},
+        {name: "AR-itus", description:"Rehabilitating individuals who are suffering with hand-eye coordination problems with the use of Augmented Reality technology.", imgsrc: "/projects/aritus.png"},
+        {name: "Teleclinic", description: "Online medical clinic for your apppointments and walk-ins, seamlessly.", imgsrc: "/projects/teleclinic.jpg"},
+        {name: "Quizzikal", description: "A competitive educational trivia web application.", imgsrc: "/projects/quizzikal.jpg"},
       ]
     }
   },
